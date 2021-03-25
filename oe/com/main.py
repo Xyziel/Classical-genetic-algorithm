024b4939b2cd91126fe277b4d05760299b46a92b
Radosław Bujak <radobujak@wp.pl>
2021-03-25 14:24:37 +0100

    add to main.py elitism strategy

--- oe/com/main.py
+++ oe/com/main.py
@@ -2,7 +2,7 @@ from oe.com.models import *
 from oe.com.mutations import *
 from oe.com.crossovers import *
 from oe.com.selections import *
-
+from oe.com.elite import *
 
 def f(x):
     return x[0] ** 2 + x[1] ** 2
@@ -23,6 +23,7 @@ def main():
     invert_prob = 0.15  # prawd. inwersji
     population_percent = 0.1  # procent populacji w selekcji najlepszych oraz ruletce
     maximum = False  # czy maksymalizacja
+    elite_number = 5  # liczba elitarnych osobnikow
 
     population = Population()
     population.create_random_population(size, n, bits)
@@ -31,17 +32,21 @@ def main():
     values = fun.get_values_population_dec(population_decimal)
 
     selection = TheBestOnesSelection(population_percent)
-    crossover = ThreePointsCrossover(cross_prob)
+    crossover = OnePointCrossover(cross_prob)
     mutation = OneBitFlipMutation(mutate_prob)
     inversion = Inversion(invert_prob)
+    elite = ElitismStrategy(elite_number)
 
     value_in_each_it = []
 
     for i in range(i):
+        elite_population = elite.choose_n_best(population, values, maximum)
         selected_parents = selection.select_parents(population, values, maximum)
-        population = crossover.cross(selected_parents, size)
+        population = crossover.cross(selected_parents, size - elite_number)
         mutation.mutate(population)
         inversion.invert(population)
+
+        population + elite_population
 
         population_dec = population.get_population_decimal(a, b)
         values = fun.get_values_population_dec(population_dec)
@@ -50,7 +55,7 @@ def main():
             value_in_each_it.append(max(values))
         else:
             value_in_each_it.append(min(values))
-            
+
     if maximum:
         print("Wartosc max (powinna byc bliska 200): " + str(max(values)))
         print("Jak zmienia")
