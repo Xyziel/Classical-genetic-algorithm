1844484c5c20402dc9901c506e689697c52a0be6
Radosław Bujak <radobujak@wp.pl>
2021-03-25 14:22:26 +0100

    add elitism strategy impl

---
+++ oe/com/elite/ElitismStrategy.py
@@ -0,0 +1,24 @@
+from oe.com.models import Population
+
+
+class ElitismStrategy:
+
+    def __init__(self, n: int):
+        self.__n = n
+
+    def choose_n_best(self, population: Population, values: list, maximum: bool) -> Population:
+        i = 0
+        selected_parents = Population()
+        value_map = {}
+        values_copy = values.copy()
+        for chromosomes in population.get_population():
+            value_map[values_copy[i]] = chromosomes
+            i += 1
+        if maximum:
+            values_copy.sort(reverse=True)
+        else:
+            values_copy.sort()
+        for i in range(self.__n):
+            chroms = value_map[values_copy[i]]
+            selected_parents.add_chromosomes(chroms)
+        return selected_parents
